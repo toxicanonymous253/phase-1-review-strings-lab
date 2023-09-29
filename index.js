d3358b3310451ec2bf67d3c78d047bd346638b4c
toxicfrank <ngugifrancis930@gmai.com>
2023-09-29 12:37:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Frank Ngugi';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
